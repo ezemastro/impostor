c1ae4a0b0a88f5200ad8b7ad90151d42468cf67c
ezemastro <mastropietroezequiel@gmail.com>
2025-11-17 21:21:55 +0000

    Inicializar proyecto con expo

---
+++ impostor/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-6xl">Inicio</Text>
+    </View>
+  );
+}
