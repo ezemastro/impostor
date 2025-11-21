19854756f3dc1c455fb5aa6b7d30f05d96a1c582
ezemastro <mastropietroezequiel@gmail.com>
2025-11-21 15:16:19 +0000

    Mejorar la lógica de asignación de espías y actualizar las dependencias en el efecto useEffect

--- impostor/hooks/game.ts
+++ impostor/hooks/game.ts
@@ -7,17 +7,17 @@ export const useGame = () => {
   const spyCount = useCurrentGameStore((state) => state.spyCount);
   const [spyIndices, setSpyIndices] = useState<Set<number>>(new Set());
 
+  // Array de numeros del 0 a players.length - 1
   const playerIndices = Array.from({ length: players.length }, (_, i) => i);
   useEffect(() => {
+    // Mezclar el array
     playerIndices.sort(() => Math.random() - 0.5);
+    // Tomar los primeros spyCount indices
     const spyIndices = new Set(playerIndices.slice(0, spyCount));
+    // Asignar el rol a cada jugador
     setSpyIndices(spyIndices);
-  }, [players.length, spyCount, currentCard]);
+  }, [players.length, spyCount, currentCard, playerIndices]);
 
-  // Array de numeros del 0 a players.length - 1
-  // Mezclar el array
-  // Tomar los primeros spyCount indices
-  // Asignar el rol a cada jugador
   const updatedPlayers = players.map((player, index) => ({
     ...player,
     isSpy: spyIndices.has(index),
