d15103dda4da917c94972ce19f124b39fa64366c
ezemastro <mastropietroezequiel@gmail.com>
2025-11-24 19:09:17 +0000

    Agregar título para la ronda de jugador como palabra en SPECIAL_ROUNDS_TITLES

--- impostor/constants/specialRounds.ts
+++ impostor/constants/specialRounds.ts
@@ -3,6 +3,7 @@ export const SPECIAL_ROUNDS = {
   NO_SPY: "noSpy",
   ALL_SPY: "allSpy",
   RANDOM_CARDS: "randomCards",
+  PLAYER_CARD: "playerCard",
 };
 
 export const SPECIAL_ROUNDS_TITLES: Record<
@@ -13,4 +14,5 @@ export const SPECIAL_ROUNDS_TITLES: Record<
   [SPECIAL_ROUNDS.NO_SPY]: "Sin espías",
   [SPECIAL_ROUNDS.ALL_SPY]: "Todos son espías",
   [SPECIAL_ROUNDS.RANDOM_CARDS]: "Palabras aleatorias",
+  [SPECIAL_ROUNDS.PLAYER_CARD]: "Jugador como palabra",
 };
