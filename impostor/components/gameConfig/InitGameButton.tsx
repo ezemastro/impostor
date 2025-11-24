6aad8f593cc2a6d0151d1455c97516f6529e9b23
ezemastro <mastropietroezequiel@gmail.com>
2025-11-24 19:09:08 +0000

    Agregar lógica para habilitar/deshabilitar el botón de inicio de juego según la selección de categorías, jugadores y espías

--- impostor/components/gameConfig/InitGameButton.tsx
+++ impostor/components/gameConfig/InitGameButton.tsx
@@ -1,11 +1,21 @@
+import { useCurrentGameStore } from "@/stores/currentGame";
 import Button from "../Button";
 import TextButton from "../TextButton";
 import { useGameInit } from "@/hooks/initGame";
 
 export default function InitGameButton() {
   const handleSubmit = useGameInit();
+  const selectedCategories = useCurrentGameStore((state) => state.categories);
+  const selectedPlayers = useCurrentGameStore((state) => state.players);
+  const selectedSpies = useCurrentGameStore((state) => state.spyCount);
+
+  const disabled =
+    selectedCategories.length === 0 ||
+    selectedPlayers.length < 2 ||
+    selectedSpies < 1 ||
+    selectedSpies >= selectedPlayers.length;
   return (
-    <Button onPress={handleSubmit}>
+    <Button onPress={handleSubmit} disabled={disabled}>
       <TextButton>Comenzar juego</TextButton>
     </Button>
   );
