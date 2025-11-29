2ba3410b4dc35210db6748a4785f3f812c0c2bce
ezemastro <mastropietroezequiel@gmail.com>
2025-11-29 13:21:52 +0000

    Agregar dependencia de tailwind-merge y actualizar componente Button para usarla

--- impostor/components/Button.tsx
+++ impostor/components/Button.tsx
@@ -1,4 +1,5 @@
 import { PressableProps, Pressable } from "react-native";
+import { twMerge } from "tailwind-merge";
 import React from "react";
 
 interface ButtonProps {
@@ -13,11 +14,11 @@ export default function Button({
 }: ButtonProps & PressableProps) {
   return (
     <Pressable
-      className={
+      className={twMerge(
         "bg-app-primary flex-row p-4 px-6 items-center justify-center rounded-lg min-h-16 min-w-80 " +
-        (disabled ? "opacity-50" : "opacity-100 ") +
-        (className || "")
-      }
+          (disabled ? "opacity-50" : "opacity-100 ") +
+          (className || ""),
+      )}
       {...props}
     >
       {children}
