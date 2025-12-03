76f0d3ab81fa03239a787779662d6afa855018a5
ezemastro <mastropietroezequiel@gmail.com>
2025-12-03 21:23:00 +0000

    Agregar alineación centrada al texto en el componente TextButton

--- impostor/components/TextButton.tsx
+++ impostor/components/TextButton.tsx
@@ -5,7 +5,10 @@ export default function TextButton(props: TextProps) {
   return (
     <Text
       {...props}
-      className={"text-white text-3xl font-semibold " + (props.className || "")}
+      className={
+        "text-white text-3xl font-semibold text-center " +
+        (props.className || "")
+      }
     >
       {props.children}
     </Text>
